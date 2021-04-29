26d33aeefe97b40a9dbd2bab3e5dc4500e78abc1
mat37.rb <mat37.rb@gmail.com>
2021-04-29 07:23:15 +0200

    1er Commit
    Structure minimale de l'application avec injection des dépendances Maven et création des packages controller, model, repository et service
    Configuration du fichier properties

---
+++ src/test/java/com/mat37/SafetyNet_Alerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mat37.SafetyNet_Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
